62ebc2e2a01e30efa166d7538971aa24d49a3033
byulent <tatarinov@logema.org>
2020-04-27 23:29:00 +0400

    Тестирование возможностей Node GDAL

--- controllers/datasetController.js
+++ controllers/datasetController.js
@@ -6,6 +6,7 @@ var layer = dataset.layers.get(0);
 exports.index = function (req, res, next) {
 	res.render('index', {
 		title: 'Node GDAL test',
+		features: layer.features,
 		featuresNumber: layer.features.count(),
 		fields: layer.fields.getNames(),
 		extent: JSON.stringify(layer.extent),
